cf75db8cc11802f2be94bcf70e2c10459e887c1c
jahirmudzakysyah <jmudzakysyah@gmail.com>
2022-08-30 13:44:47 +0700

    first commit

---
+++ application/views/umum/umum_view.php
@@ -0,0 +1,45 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>view</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <style>
+        body{
+            background-color: grey;
+        }
+        header{
+            width: 100%;
+            margin: auto;
+            
+        }
+        
+    </style>
+</head>
+<body>
+    <div class="header">
+    <h1>Nama Kelompok</h1>
+    <ol>
+        <li>Satria</li>
+        <li>Ardi</li>
+        <li>Wildan</li>
+        <li>Jahir</li>
+    </ol>
+    </div>
+    
+    
+    <nav class="navbar bg-light">
+  <div class="container-fluid">
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+</body>
+</html>
